2adb93caf0ac921e39018a8a67b8cc260b81e487
LiuEhe <126216347+LiuEhe@users.noreply.github.com>
2023-05-08 16:14:54 +0800

    Update 1_Xy.py
    优化进度条方式，以及代码复杂度，变得更加简单

--- 1_Xy.py
+++ 1_Xy.py
@@ -10,6 +10,7 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 from tqdm import tqdm
 import time
+import glob
 
 # 1. 读取配置文件中的信息
 train_dir = get("train") # 获取 训练数据路径
@@ -22,56 +23,36 @@ new_size = get("new_size") # 获取 新图像大小元组, 注意: 必须包含h
 print("# 读取训练数据并进行预处理，") 
 # TODO
 
-
+#创建X，y列表
 X = []
 y = []
 
-#读取图片
-for file_name in os.listdir(train_dir):
-    img = preprocess_image(train_dir+'/'+ file_name, new_size)
-    X.append(img)
+#获取符样式列表
+char_styles = get("char_styles")  # 获取 字符样式列表，注意: 必须是列标
 
-#创建映射
-#mapping = {'楷书': 1, '篆书': 2, '行书': 3, '草书': 4, '隶书': 5}
-mapping = {'草书': 1, '楷书': 2, '隶书': 3, '行书': 4, '篆书': 5}
-#mapping = {'1': '楷书', 2:'篆书', 3:'行书', 4:'草书', 5:'隶书'}
 
-#读取标签
-for file_name in os.listdir(train_dir):
-    if file_name.endswith('.jpg'):
-        y.append(file_name.split('_')[1])
+# 使用glob.glob函数查找符合条件的文件，并将结果保存到image_files列表中
+#递归列表
+image_files = [glob.glob(f"{train_dir}/train_{category}*") for category in char_styles]
 
+
+for i in range(5):
+    for element in tqdm(image_files[i], desc=f"处理 {char_styles[i]} 图像", unit="bit"):
+        label = element.split('_')[1]
+        X.append(preprocess_image(element, new_size))
+        y.append(char_styles.index(label))
+
+#转换成np数组
 X = np.array(X)
 y = np.array(y)
+#print(y)
 
+#转换成float64类型
 X = X.astype(np.float64)
 
-#print(y)
-#print(X)
-# 使用tqdm创建进度条，并遍历linspace_list中的元素
-for i in range(5):
-    if i == 0:   
-        for element in tqdm(y[0:1000],desc=f'处理  {y[0]}  图像', unit="每秒的单位"):
-            time.sleep(0.001)
+#转换成int64类型
+y = y.astype(np.int64)
 
-    if i == 1:   
-        for element in tqdm(y[1000:2000],desc=f'处理  {y[1000]}  图像', unit="每秒的单位"):
-            time.sleep(0.001)
-
-    if i == 2:   
-        for element in tqdm(y[2000:3000],desc=f'处理  {y[2000]}  图像', unit="每秒的单位"):
-            time.sleep(0.001)   
-
-    if i == 3:   
-        for element in tqdm(y[3000:4000],desc=f'处理  {y[3000]}  图像', unit="每秒的单位"):
-            time.sleep(0.001)  
-
-    if i == 4:   
-        for element in tqdm(y[4000:5000],desc=f'处理  {y[4000]}  图像', unit="每秒的单位"):
-            time.sleep(0.001)           
-
-vectorized_mapper = np.vectorize(lambda x: mapping[x])
-y = vectorized_mapper(y).astype(np.int64)
 
 # 3. 分割测试集和训练集
 print("# 将数据按 80% 和 20% 的比例分割")
